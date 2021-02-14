a028ccfcb164c332f70ee1d5cc7510b32744d621
Valentin Mirchev <valentin97mirchev@gmail.com>
2021-02-14 04:21:14 +0000

    flights app finished, customized admin page

--- SQL/airlines/flights/views.py
+++ SQL/airlines/flights/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(id=flight_id)
+        passenger = Passenger.objects.get(id=int(request.POST['passenger']))
+        passenger.flights.add(flight)
+        return redirect('flights:flight', flight_id)
